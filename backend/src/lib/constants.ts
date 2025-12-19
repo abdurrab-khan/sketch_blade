37f69b6cc3677bb4ebf17270df1eabed076e16dd
Abdur Rab Khan <146613551+abdurrab-khan@users.noreply.github.com>
2025-12-19 10:53:17 +0530

    Delete backend directory

--- backend/src/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-const MONGO_NAME = "excalidraw";
-const CACHE_EXPIRATION = 60 * 60 * 24;
-
-export { MONGO_NAME, CACHE_EXPIRATION };
-
-// MESSAGES
--- backend/src/utils/AsyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default function AsyncHandler(fn: Function) {
-  return function (req: Request, res: Response, next: NextFunction) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
